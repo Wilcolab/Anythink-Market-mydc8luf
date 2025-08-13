56047f7166d2c01ebc89aeeaf3a3e17ff4dafe30
WilcoApp <hello@wilco.gg>
2025-08-13 09:29:34 +0000

    Initial commit

--- basic_prompt.js
+++
@@ -1,9 +0,0 @@
-/**
- * Converts a given string to camelCase.
- * Words are split by spaces, underscores, or hyphens.
- * Example: "
- * Example: "convert_to-camel case" => "convertToCamelCashello world" => "helloWorld"e"
- *
- * @param {string} str - The input string to convert.
- * @returns {string} The camelCase version of the input string.
- */
